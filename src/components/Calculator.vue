fd895d045d14a6fec70e68db887767dae1251f99
Arina <rina.runet@gmail.com>
2021-09-19 15:09:37 +0300

    HW_2

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -8,6 +8,9 @@
        <p class="calc-func">=</p>
        <p class="calc-result">{{ result }}</p>
      </div>
+     <div class="message" v-if="message !== null">
+         <p>{{message}}</p>
+     </div>
      <div class="keyboard">
         <button @click="addition">+</button>
         <button @click="subtraction">-</button>
@@ -17,6 +20,24 @@
         <button @click="wholeDivision">//</button>
         <button @click="remainder">%</button>
      </div>
+     <div class="checkbox">
+        <input type="checkbox" id="show-keyboard" v-model="show">
+        <label for="show-keyboard">Отобразить экранную клавиатуру</label>
+     </div>
+     <div v-if="this.show === true">
+         <ul class="keyboard-buttons-list" id="keys">
+            <li v-for="item in keys" :key="item.value">
+                <button class="keyboard-button" @click="displayKeyboard(item.value), value=item.value" >{{ item.value }}</button>
+            </li>
+            <button class="keyboard-button" @click="deleteLastSymbol()">&#8592;</button>
+        </ul>
+     </div>
+     <div class="change">
+        <input type="radio" id="operand1" value="operand1" v-model="picked">
+        <label for="one">Operand 1</label>
+        <input type="radio" id="operand2" value="operand2" v-model="picked">
+        <label for="two">Operand 2</label>
+     </div>
   </div>
 </template>
 
@@ -24,10 +45,25 @@
 export default {
   name: 'Calculator',
   data: () => ({
-    operand1: null,
-    operand2: null,
+    operand1: '',
+    operand2: '',
     result: 0,
     operator: '',
+    show: null,
+    keys: [
+        {value: '1'},
+        {value: '2'},
+        {value: '3'},
+        {value: '4'},
+        {value: '5'},
+        {value: '6'},
+        {value: '7'},
+        {value: '8'},
+        {value: '9'},
+    ],
+    picked: '',
+    value: '',
+    message: null,
   }),
   methods: {
     addition() {
@@ -39,8 +75,9 @@ export default {
       this.operator='-';
     },
     division() {
-      this.result = this.operand1 / this.operand2;
-      this.operator='/';
+      this.message = null;
+      this.operator = '/';
+      this.operand2 !== 0 ? this.result = this.operand1 / this.operand2 :  this.message = "Пожалуйста, не делите на 0"
     },
     multiplication() {
       this.result = this.operand1 * this.operand2;
@@ -51,12 +88,28 @@ export default {
       this.operator='^';
     },
     wholeDivision() {
-      this.result = (this.operand1 - this.operand1 % this.operand2) / this.operand2;
+      this.message = null;
       this.operator='//';
+      this.operand2 !== 0 ? this.result = (this.operand1 - this.operand1 % this.operand2) / this.operand2 : this.message = "Пожалуйста, не делите на 0"
     },
     remainder() {
       this.result = this.operand1 % this.operand2;
       this.operator='%';
+    },
+    displayKeyboard(value){
+      this.picked === 'operand1' ? this.operand1 = this.operand1 + value : this.operand2 = this.operand2 + value;
+    },
+    deleteLastSymbol(){
+      if(this.picked === 'operand1'){
+          this.operand1 = String(this.operand1);
+          let newData = this.operand1.slice(0, -1);
+          this.operand1 = newData;
+      }
+      else{
+          this.operand2 = String(this.operand2);
+          let newData = this.operand2.slice(0, -1);
+          this.operand2 = newData;
+      }
     }
   },
 }
